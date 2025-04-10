64bbf1f29babfd00e3a7595e22aba65f782ae719
Sarah Proctor <sproctor950@gmail.com>
2025-04-10 15:48:49 -0700

    🎁 Adds Document resource for flexible metadata

---
+++ app/forms/document_form.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Generated via
+#  `rails generate hyku_knapsack:work_resource Document --flexible`
+class DocumentForm < Hyrax::Forms::ResourceForm(Document)
+end
---
+++ app/models/document.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Generated via
+#  `rails generate hyku_knapsack:work_resource Document --flexible`
+class Document < Hyrax::Work
+  include Hyrax::ArResource
+  include Hyrax::NestedWorks
+  
+  include IiifPrint.model_configuration(
+    pdf_split_child_model: GenericWorkResource,
+    pdf_splitter_service: IiifPrint::TenantConfig::PdfSplitter
+  )
+end 
