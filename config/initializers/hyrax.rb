5b4226904571ec2511adeb8921c93870517393fa
Shana Moore <shana.lavina.moore@gmail.com>
2025-11-10 13:39:26 -0800

    Update submodule

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -4,6 +4,9 @@
 
 Rails.application.config.after_initialize do
   Hyrax.config do |config|
+    config.flexible = true # ActiveModel::Type::Boolean.new.cast(ENV.fetch('HYRAX_FLEXIBLE', false))
+
+    # Set default profile path
     config.default_m3_profile_path = HykuKnapsack::Engine.root.join('config', 'metadata_profiles', 'm3_profile.yaml')
     # Clears the default registered concerns and adds in the concerns specified in the m3_profile.yaml
     config.instance_variable_set(:@registered_concerns, [])
